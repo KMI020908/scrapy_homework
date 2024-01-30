6bffbbe0d7dc9fbf39ac1d9d3f89f568d5aaf323
Mikhail <mikhailkochergin08@gmail.com>
2024-01-30 21:06:16 +0300

    added pipelines for geners, countries, directors

--- homework/homework/pipelines.py
+++ homework/homework/pipelines.py
@@ -6,8 +6,24 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+import re
 
-
-class HomeworkPipeline:
+class MoviePipeline:
     def process_item(self, item, spider):
+        adapter = ItemAdapter(item)
+        forbidden_words = ['', ',', 'и', '/', '-', '[', ']', '.рус', '(англ.)', '(', ')', '|', 'править']
+        forbidden_patterns = [r'\[.*\]', r'{']
+        proc_items_lst = ['genre', 'country', 'director']
+        for proc_item in proc_items_lst:
+            item_words = adapter.get(proc_item)
+            words_to_remove = []
+            if item_words:
+                item_words = [word for word in item_words if word not in forbidden_words]
+                for pattern in forbidden_patterns:
+                    for word in item_words:
+                        if re.search(pattern, word):
+                            words_to_remove.append(word)
+                item_words = [word for word in item_words if word not in words_to_remove]
+                adapter[proc_item] = list(set(item_words))
+
         return item
