d2a8a242b636a7d58eb482b26ed97b655953413a
Mikhail <mikhailkochergin08@gmail.com>
2024-01-30 04:53:47 +0300

    improved years_parse and movies_parse

--- homework/homework/spiders/wiki_movies_2.py
+++ homework/homework/spiders/wiki_movies_2.py
@@ -18,33 +18,40 @@ class WikiMovies2Spider(scrapy.Spider):
 
     def start_requests(self) -> Iterable[Request]:
         URL = self.start_urls[0]
-        yield scrapy.Request(url=URL, callback=self.year_parse)  
+        yield scrapy.Request(url=URL, callback=self.years_parse)  
 
-    def year_parse(self, response):
-        for selector in response.css("div.CategoryTreeItem"):
-            title = selector.css("a::attr(title)").extract()[0] 
-            if re.search(r'по', title) is None:
-                next_page = "https://" + self.allowed_domains[0] + selector.css("a::attr(href)").extract_first()
+    def years_parse(self, response):
+        years_groups = response.css('div.CategoryTreeItem > a')
+        for group in years_groups:
+            if 'по' not in group.css('::text'):
+                next_page = group.attrib['href']
                 if next_page:
-                    yield response.follow(url=next_page, callback=self.movie_parse)
+                    yield response.follow(url=response.urljoin(next_page), callback=self.movies_parse)
+    
+    def movies_parse(self, response):
+        year = response.css('#mw-content-text > div.mw-content-ltr.mw-parser-output > table > tbody > tr > th::text').get()
+        if year:
+            year = year.replace('\n', '')
+        movie_groups = response.css('#mw-pages > div > div > div')
+        for group in movie_groups:
+            if group.css('h3::text').get() != '*':
+                for movie in group.css('ul > li'):
+                    movie_page, title = [*movie.css('a').attrib.values()]
+                    if movie_page:
+                        yield response.follow(url=response.urljoin(movie_page), callback=self.movie_page_parse, meta={'year': year, 'title': title})
 
-    def movie_parse(self, response):
-        for selector in response.css("div.mw-category-group"):
-            movie_pages = selector.css("ul > li > a::attr(href)").extract()
-            for page in movie_pages:
-                if page:
-                    page = "https://" + self.allowed_domains[0] + str(page)
-                    yield response.follow(url=page, callback=self.movie_page_parse)
-        next_page = response.css("#mw-pages > a::attr(href)").extract()
-        if next_page:
-            if response.css("#mw-pages > a::text").extract()[-1] != "Предыдущая страница":
-                yield response.follow(url=response.urljoin(next_page[-1]), callback=self.movie_parse)
+        next_page_buttons = response.css('#mw-pages > a')
+        visited_next_page = False
+        for button in next_page_buttons:
+            if (not visited_next_page) and button.css('::text').get() == "Следующая страница":
+                next_page = button.attrib['href']
+                if next_page:
+                    visited_next_page = True
+                    yield response.follow(url=response.urljoin(next_page), callback=self.movies_parse)
 
     def movie_page_parse(self, response):
-        title = response.css("span.mw-page-title-main::text").extract()
-        if not title:
-            title = response.css("#firstHeading > i::text").extract()
-        genre, director, country, year = None, None, None, None
+        
+        genre, director, country = None, None, None
         for selector in response.css("div.mw-content-ltr.mw-parser-output > table > tbody > tr"):
             if selector.css("th.plainlist > a::text").extract_first() in self.attr_matches['genre']:
                 for genre_selector in selector.css("td.plainlist"):
@@ -54,22 +61,16 @@ class WikiMovies2Spider(scrapy.Spider):
                 for director_selector in selector.css("td.plainlist"):
                     director = director_selector.css("::text").extract()
                     director = [el for el in director if el not in self.forbidden_words]
-            if selector.css("th.plainlist > a::text").extract_first() in self.attr_matches['year'] or selector.css("th.plainlist::text").extract_first() in self.attr_matches['year']:
-                for year_selector in selector.css("td.plainlist"):
-                    year = year_selector.css("::text").extract()
-                    if year[-1] in [f'[{n}]' for n in range(10)]:
-                        year = year[-2][-4:]
-                    else:
-                        year = year[-1][-4:]
             if selector.css("th.plainlist::text").extract_first() in self.attr_matches['country']:
                 for country_selector in selector.css("td.plainlist"):
                     country = country_selector.css("::text").extract()
                     country = [el for el in country if el not in self.forbidden_words]
+      
         yield {
-            'title': title, 
+            'title': response.meta['title'],
             'genre': genre,
             'director': director,
             'country': country,
-            'year': year
+            'year': response.meta['year']
         }
                     
